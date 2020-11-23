4f6aa4bf35b22eb16839e6d130b1e2f474559532
MarketaR0 <marketa.rollerova@scioskola.cz>
2020-11-23 09:55:23 +0100

    Factorial added without mistake

--- src/AdvFunctions.py
+++ src/AdvFunctions.py
@@ -7,12 +7,15 @@ def int_division(num1, num2):
 
 # Returns factorial of given number
 def factorial(num):
-    if num > 0:
-       return (math.factorial(num))
-    elif num == 0:
-        return 1
+    if num == int:
+        if num > 0:
+            return (math.factorial(num))
+        elif num == 0:
+            return 1
+        else:
+            print('We can not make factorial of negative number. Please enter a valid input')
     else:
-        print('We can not make factorial of negative number. Please enter a valid import')
+        print('We need a natural number to count a factorial. Please enter a valid input.')
 
 # Returns all solutions of given quadratic equation
 def quadratic_eq(qdr_coef, lin_coef, const):
